57cb65fde24419b13b9c1a21d4bff94038f43ff3
wangjinjing1984 <147446520@qq.com>
2022-05-13 09:07:24 +0800

    https://github.com/wangjinjing1984/HyTest/blob/master/demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,6 @@
 import os
 print(os.getcwd())
 print('This is second commit')
-<<<<<<< HEAD
 print('pull requests remote github reposity')
-
-=======
 print('vscode remote this message.')
->>>>>>> e984b408014c9f7f0e1a68de395684ec59bebe69
+
