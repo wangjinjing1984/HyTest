fb756b6b01943e124d2b165ba78fe38bc8b19bc7
wangjinjing1984 <147446520@qq.com>
2022-05-13 17:43:29 +0800

    the lastest change demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ def main():
     print(os.getcwd())
     print('This is second commit')
     print('pull requests remote github reposity')
-p   print('vscode remote this message.')
+    print('vscode remote this message.')
     print("the new push...")
     print("this is ths last change to github...")
 
